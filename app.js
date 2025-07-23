9f85d5c5c154e5a775e8aab29749c8db95c1ee2c
Yudd <yudaputera11@gmail.com>
2025-07-23 20:29:28 +0700

    Changing from ejs to html + tailwind

--- app.js
+++ app.js
@@ -11,7 +11,10 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
-app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
@@ -19,9 +22,9 @@ mongoose.connect(process.env.MONGO_URI, {
 }).then(() => console.log('MongoDB connected'))
   .catch(err => console.error(err));
 
-app.use('/', redirectRoutes);
-app.use('/', shortenRoutes);
-app.use('/', dataRoutes);
+app.use(shortenRoutes);
+app.use(redirectRoutes);
+app.use(dataRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
